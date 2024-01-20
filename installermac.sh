2815b2f7d69a767aec769c6935f880d672494574
AntoP96 <apaolino96@gmail.com>
2024-01-20 14:27:03 +0100

    fix

--- installermac.sh
+++ installermac.sh
@@ -12,7 +12,7 @@ docker volume create VolumeT8 || echo "Errore nella creazione del volume"
 docker network create global-network || echo "Errore nella creazione della rete"
 
 # Definizione dei percorsi delle directory da visitare
-list=("T8-G21/Progetto_SAD_GRUPPO21_TASK8/Progetto_def/opt_livelli/Prototipo2.0" "T1-G11/applicazione/manvsclass" "T23-G1" "T4-G18" "T5-G2/t5" "T6-G12/T6" "T7-G31/RemoteCCC" "T9-G19/Progetto-SAD-G19-master" "api_gateway" "ui_gateway")
+list=("T8-G21/Progetto_SAD_GRUPPO21_TASK8/Progetto_def/opt_livelli/Prototipo2.0" "T1-G11/applicazione/manvsclass" "T23-G1" "T4-G18" "T5-G2/t5" "T6-G12/T6" "T7-G31/RemoteCCC" "T9-G19/Progetto-SAD-G19-master" "api_gateway" "ui_gateway" "fastapi")
 
 # Ciclo attraverso le directory specificate
 for dir in "${list[@]}"; do
